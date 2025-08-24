44268914cc92e7821110ba4577d42dbe87dad9fe
Ayush Raj <rajayush3182003@gmail.com>
2025-08-24 22:07:38 +0530

    SignUp Route

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -3,7 +3,7 @@ import UserModel from '@/model/User';
 import bcrypt from 'bcryptjs';
 import { sendVerificationEmail } from '@/helpers/sendVerificationEmail';
 
-export async function POST(request: Request) {
+async function POST(request: Request) {
   await dbConnect();
 
   try {
@@ -96,3 +96,5 @@ export async function POST(request: Request) {
     );
   }
 }
+
+export { POST };
