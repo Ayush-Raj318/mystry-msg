35d2313876953a717f7d3a725227aaa139a4f2f6
Ayush Raj <rajayush3182003@gmail.com>
2025-08-24 11:21:31 +0530

    Schemas and Zod Validations

---
+++ src/schemas/signupSchema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const usernameValidation = z
+  .string()
+  .min(4, 'Username must be at least 4 characters')
+  .max(10, 'Username must be no more than 10 characters')
+  .regex(/^[a-zA-Z0-9_]+$/, 'Username must not contain special characters');
+
+const signUpSchema = z.object({
+  username: usernameValidation,
+
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+  .string()
+  .min(6, { message: "Password must be at least 6 characters" })
+  .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+  .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+  .regex(/[@$!%*?&]/, { message: "Password must contain at least one special character (@$!%*?&)" }),
+});
+
+export default signUpSchema;
